d4a9861be0c28b39c922753662b90a659e0e7a7a
Asus <vaxa2320@gmail.com>
2023-12-14 19:58:56 +0300

    user1

---
+++ src/main/java/by/itclass/controllers/order/CartController.java
@@ -0,0 +1,12 @@
+package by.itclass.controllers.order;
+
+import by.itclass.controllers.AbstractController;
+import jakarta.servlet.annotation.WebServlet;
+
+import static by.itclass.constants.ApplicationConstants.CART_CONTROLLER;
+
+
+@WebServlet(value = CART_CONTROLLER)
+public class CartController extends AbstractController {
+
+}
